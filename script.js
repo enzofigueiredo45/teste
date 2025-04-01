502f0ac7fe49f7c5e1886aec7e927f55ab5ec768
Enzo Figueiredo <enzofigueredo45@gmail.com>
2025-03-31 22:40:12 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var typed = new Typed(".multiple-text", {
     strings: ["Enzo Figueiredo", "Paulista", "Desenvolvedor FrontEnd", "Técnico em Mecatrônica"],
-    typeSpeed: 100, 
-    backSpeed: 100, 
-    backDelay: 1000,
-    loop: true
-})
+    typeSpeed: 100,   // Velocidade de digitação (em milissegundos)
+    backSpeed: 100,   // Velocidade de apagamento
+    backDelay: 1000,  // Atraso antes de começar a apagar o texto
+    loop: true        // Continuar o loop da animação
+});
